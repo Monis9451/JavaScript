0d11aa6f477a6b45f09072480d166f55949dd96c
Monis9451 <mianmonishussain786@gmail.com>
2024-07-01 19:15:55 +0500

    initial push

---
+++ practice.js
@@ -0,0 +1,31 @@
+// console.log("Hello World");
+// a=2;
+// b=10;
+// c="Monis"
+// d=true
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+
+// //Performing operators in it
+
+// let add = a + b; //addition
+// console.log("The sum of ",a," and ",b,"is           :", add);
+
+// let sub=a-b; //subtraction
+// console.log("The subtraction of ",a," and ",b,"is   :", sub);
+
+// let div=a/b; //division
+// console.log("The division of ",a," and ",b,"is      :", div);
+
+// let mul=a*b; //multiplication
+// console.log("The multiplication of ",a," and ",b,"is:", mul);
+
+// let rem=a%b; //remainder
+// console.log("The remainder of ",a," and ",b,"is     :", rem);
+
+// let exp=a**b; //exponent
+// console.log("The exponent of ",a," power ",b,"is    :", exp);
+console.log(0/0);
