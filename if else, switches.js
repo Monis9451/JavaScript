a4e353366ded941fd599d1116744dfbc6503e07f
Monis9451 <mianmonishussain786@gmail.com>
2024-07-03 20:32:57 +0500

    pratice loops and link an html file with js file

--- if else, switches.js
+++ if else, switches.js
@@ -18,30 +18,66 @@
 
 //Switch statement
 
-let day = 38745 
-switch(day)
-{
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");
-        break;
-    case 4:
-        console.log("Thrusday");
-        break;
-    case 5:
-        console.log("Friday");
-        break;
-    case 6:
-        console.log("Saturday");
-        break;
-    case 7:
-        console.log("Sunday");
-        break;
-    default:
-        console.log("Enter a valid day number");
-}
+// let day = 38745 
+// switch(day)
+// {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thrusday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Enter a valid day number");
+// }
+// alert("THis is a simple alert!");
+// console.error("This is an error")
+// let FN = prompt("Enter First name: ")
+// let LN = prompt("Enter last name: ")
+// alert(`My name is:  ${FN + " " + LN}`)
+// msg = "         Monis   hussain   ";
+// console.log(msg);
+// msg  = msg.trim();
+// console.log(msg);
+// console.log(msg.indexOf("i"));
+// console.log(msg.indexOf("huss"));
+// console.log(msg.indexOf("n"));
+// console.log(msg.slice(8,10));
+// console.log(msg.replace("M","m"));
+// console.log(msg.replace("Z","m"));
+// console.log(msg.repeat(3));
+// let arr1 = ["monis","ali","asad"]
+// let arr2 = ["amir","faizan","monis"]
+
+// console.log(arr1.concat(arr2))
+// console.log(arr1.reverse())
+
+//printing all odd numners from 1 to 15 using loop
+for(let i=1;i<=15;i++)
+    {
+        if((i%2)!=0)
+            {
+                console.log(i)
+            }
+    }
+for(let i=1;i<=15;i+=2)
+    {
+        console.log(i)
+    }
+
+
