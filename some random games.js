276e1b283705819b2f580fba2d5d49a4d01faf2f
Monis9451 <mianmonishussain786@gmail.com>
2024-07-04 13:16:00 +0500

    make a table for a given number

---
+++ some random games.js
@@ -0,0 +1,39 @@
+// let win = false;
+// while(win == false)
+//     {
+//         let num = Math.floor(Math.random()*10);
+//         let yourNum = prompt("Guess the random number between 0-10: ")
+//         console.log(`Your entered number: ${yourNum}`)
+//         console.log(`Number to guess is: ${num}`)
+//         if(num == yourNum)
+//             {
+//                 console.log("Congrats! You guess the number correctly.")
+//                 win = true;
+//             }
+//     }
+
+//Making a multiplication table
+
+// function multiplicationTable(num)
+// {
+//     console.log(`${num} x 1 = ${num*1}`)
+//     console.log(`${num} x 2 = ${num*2}`)
+//     console.log(`${num} x 3 = ${num*3}`)
+//     console.log(`${num} x 4 = ${num*4}`)
+//     console.log(`${num} x 5 = ${num*5}`)
+//     console.log(`${num} x 6 = ${num*6}`)
+//     console.log(`${num} x 7 = ${num*7}`)
+//     console.log(`${num} x 8 = ${num*8}`)
+//     console.log(`${num} x 9 = ${num*9}`)
+//     console.log(`${num} x 10 = ${num*10}`)
+// }
+// num = prompt("Enter a number for its multiplication table: ")
+// multiplicationTable(num);
+
+let names = "monis"
+function changeName()
+{
+    names = "ali";
+}
+changeName();
+console.log(names);
