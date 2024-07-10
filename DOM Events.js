cfd071e95d8f340c227bc1e73a38c0457d7177df
Monis9451 <mianmonishussain786@gmail.com>
2024-07-10 13:13:34 +0500

    Made a random color generator

--- DOM Events.js
+++ DOM Events.js
@@ -1,2 +1,42 @@
-let btn = document.querySelector("button")
-console.dir(btn)
+// let btn = document.querySelectorAll("button")
+
+// for(button of btn)
+// {
+//     // button.addEventListener("click", alertFunc)
+//     // button.addEventListener("click", helloFunc)
+//     button.addEventListener("dblclick", dblFunc)
+// }
+// function alertFunc()
+// {
+//     alert("Button is clicked")
+// }
+// function helloFunc()
+// {
+//     alert("Hello")
+// }
+// function dblFunc()
+// {
+//     console.log("You double clicked")
+// }
+
+//Random color generator
+button1 = document.querySelector("button")
+button1.addEventListener("click", function()
+{
+    let h3 = document.querySelector("h3")
+    rColor = getRandomColor()
+    h3.innerText = rColor
+    
+    let div1 = document.querySelector("div")
+    div1.style.backgroundColor = rColor
+    console.log("Color updated")
+})
+
+function getRandomColor()
+{
+    let red = Math.floor(Math.random()*255)
+    let green = Math.floor(Math.random()*255)
+    let blue = Math.floor(Math.random()*255)
+    let color = `rgb(${red}, ${green}, ${blue})`
+    return color
+}
