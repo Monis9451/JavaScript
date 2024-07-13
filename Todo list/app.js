02767c7836e539fe4d047340511900b0d72ee6c2
Monis9451 <mianmonishussain786@gmail.com>
2024-07-13 18:36:29 +0500

    Todo List completes

--- Todo list/app.js
+++ Todo list/app.js
@@ -5,9 +5,35 @@ let ul = document.querySelector("ul")
 button.addEventListener("click",function()
 {
     let item = document.createElement("li")
-    item.innerText = input.value
+    item.innerText = input.value + " "
     let delbtn = document.createElement("button")
     delbtn.innerText = "Delete"
+    delbtn.classList.add("del")
+    item.appendChild(delbtn)
     ul.appendChild(item)
     input.value = ""
+})
+
+
+ul.addEventListener("click", function(event)
+{
+    if(event.target.nodeName == "BUTTON" && event.target.className == "del")
+    {
+        event.target.parentElement.remove()
+    }
+})
+
+document.addEventListener("keydown",function(event)
+{
+    if(event.key === "Enter")
+    {
+        let item = document.createElement("li")
+        item.innerText = input.value
+        let delbtn = document.createElement("button")
+        delbtn.innerText = "Delete"
+        delbtn.classList.add("del")
+        item.appendChild(delbtn)
+        ul.appendChild(item)
+        input.value = ""
+    }
 })
