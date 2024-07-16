530af58a75c280b0f24c57d0e1ed63573c883a1b
Monis9451 <mianmonishussain786@gmail.com>
2024-07-16 16:38:48 +0500

    Practice promises

--- call_stack.js
+++ call_stack.js
@@ -18,23 +18,85 @@
 
 //Making a call  stack hell
 
-h1 = document.querySelector("h1")
-function colorChange(color, delay, newFunc)
-{
-    setTimeout(()=>{
-        h1.style.color = color
-        if(newFunc)
-            {
-                newFunc()
-            }
-    },delay)
+// h1 = document.querySelector("h1")
+// function colorChange(color, delay, newFunc){
+//     setTimeout(()=>{
+//         h1.style.color = color
+//         if(newFunc)
+//             {
+//                 newFunc()
+//             }
+//     },delay)
     
+// }
+
+// colorChange("red",1000,()=>{
+//     colorChange("blue",1000,()=>{
+//         colorChange("green",1000,()=>{
+//             colorChange("yellow",1000)
+// })
+// })
+// })
+
+// function saveToDB(data)
+// {
+//     return new Promise((success, failure)=>{
+//     let internetSpeed = Math.floor(Math.random()*10)+1
+//     if(internetSpeed>4){
+//         success("data was saved")
+//     }else{
+//         failure("retry, data not saved")
+//     }
+//     })
+// }
+
+// // let request = saveToDB("Monis Hussain")//request = promise object
+// saveToDB("Monis Hussain")
+// .then((result)=>{
+//     console.log("Result: ",result)
+//     console.log("Promise was resolved, Data 1 saved")
+//     return saveToDB("Umair nadeem")
+// })
+// .then((result)=>{
+//     console.log("Result: ",result)
+//     console.log("Promise was resolved, Data 2 saved")
+//     return saveToDB("Another name")
+// })
+// .then((result)=>{
+//     console.log("Result: ",result)
+//     console.log("Promise was resolved, Data 3 saved")
+// })
+// .catch((error)=>{
+//     console.log("Error: ",error)
+//     console.log("Promise was rejected")
+// })
+
+//Callback hell for color code
+
+
+h1 = document.querySelector("h1")
+function colorChange(color,delay){
+    return new Promise((success)=>{
+        setTimeout(()=>{
+            h1.style.color = color
+            success("Color changed successfully")
+        },delay)
+    })
 }
 
-colorChange("red",1000,()=>{
-    colorChange("blue",1000,()=>{
-        colorChange("green",1000,()=>{
-            colorChange("yellow",1000)
+colorChange("red",1000)
+.then(()=>{
+    console.log("Text converted to red color")
+    return colorChange("green",1000)
 })
+.then(()=>{
+    console.log("Text converted to green color")
+    return colorChange("lightblue",1000)
 })
+.then(()=>{
+    console.log("Text converted to lightblue color")
+    return colorChange("yellow",1000)
+})
+.then(()=>{
+    console.log("Text converted to lightblue color")
 })
